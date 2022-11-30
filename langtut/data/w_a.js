46b95d9cded81e2bfee4b25528f7996c4232be0f
greencar77 <x>
2022-11-30 09:20:43 +0200

    db

--- langtut/data/w_a.js
+++ langtut/data/w_a.js
@@ -4,37 +4,4 @@ let words = [
   "v": "crest",
   "t": "n",
   "trans": "pušķītis, pati augša",
-},
-{
-  "id": "2173657",
-  "v": "pulley",
-  "t": "n",
-  "trans": "trīsis",
-},
-{
-  "id": "2173243",
-  "v": "wedge",
-  "t": "n",
-  "trans": "ķīlis",
-},
-{
-  "id": "2157519",
-  "v": "dismal",
-  "t": "a",
-  "trans": "depresiju izraisošs",
-  "p": "+1",
-},
-{
-  "id": "2190843",
-  "v": "here you are",
-  "t": "ph",
-  "trans": "lūdzu! (dodot)",
-  "p": "+1",
-},
-{
-  "id": "2130009",
-  "v": "astray",
-  "t": "av",
-  "p": "+1",
-  "trans": "nost no ceļa",
 }];
