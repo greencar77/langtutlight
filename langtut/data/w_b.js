cea0760237e719af4a1511ce30782adc907e77a0
greencar77 <x>
2022-11-30 09:18:47 +0200

    db

--- langtut/data/w_b.js
+++ langtut/data/w_b.js
@@ -1,5 +1,27 @@
 let sentences = [
 {
+  "id": "2173742",
+  "v": "Some challenging pulls can require re-rigging your pulley multiple times to change angles slowly.",
+  "v-2173657": "Some challenging pulls can require re-rigging your %trīsi% multiple times to change angles slowly.",
+  "com": "Wes Siler, Outside Online, 28 Sep. 2022, merriam-webster",
+},
+{
+  "id": "2173413",
+  "v": "The crowd formed itself into a wedge.",
+  "v-2173243": "The crowd formed itself into a %ķīlis%.",
+},
+{
+  "id": "2157226",
+  "v": "Without the encouragement of your father the world is a dismal place.",
+  "v-2157519": "Without the encouragement of your father the world is a %*% place.",
+  "com": "youtube - Things you must know as a father. JPeterson",
+},
+{
+  "id": "2190044",
+  "v": "Here you go sir. I’m sure she will love these.",
+  "v-2190843": "%% sir. I’m sure she will love these.",
+},
+{
   "id": "2130145",
   "v": "The money seems to have gone astray.",
   "v-2130009": "The money seems to have gone %neceļos%.",
